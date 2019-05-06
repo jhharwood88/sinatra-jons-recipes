68ab45d079ea2e504342c46aed78b1380e20c86d
Jon <jhharwood88@gmail.com>
2019-05-06 19:02:19 -0400

    added routes for users login/create

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,12 @@
 class RecipesController < ApplicationController
+  get '/recipes/index' do 
+  	# list all the available recipes to choose from
+  end
+
+  get '/recipes/:id' do 
+  	# display particular recipe/ link to edit / link to delete
+  end
+
   get '/recipes/new' do
     # load the new recipe form
   end
@@ -6,4 +14,17 @@ class RecipesController < ApplicationController
   post '/recipes' do
     # receive the params from the form submission and create a new recipe
   end
+
+  get '/recipes/:id/edit' do 
+  	# load the edit form / load button for recipe deletion
+  end
+
+  patch '/recipes/:id/edit' do 
+  	# update the given recipe / check to make sure forms arent left blank
+  end
+
+  delete '/recipes/:id/edit' do 
+  	# remove the selected recipe from the DB
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
 
+	get '/' do 
+		# login/out or create a new user, homepage
+		erb :'/users/index'
+	end
+
+	get '/users/login' do 
+		# gives user login form
+		erb :'/users/login'
+	end
+
+	get '/users/new' do
+    # load the new user form
+    	erb :'/users/new'
+	end
+
+	post '/users' do
+	  # receive the params from the form submission and create a new user
+	  	@user = User.create(params[:user])
+   
+    redirect "users/#{@user.id}"
+
+	end
+
+	get '/users/:id' do 
+		# displays the user page with their recipes, link to edit/add
+		erb :'/users/show'
+	end
+
 end
